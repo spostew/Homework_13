78c6da30e370c5ab56af0c409fa49d81375a1476
spostew <spencerunitt0@gmail.com>
2021-12-17 18:17:00 -0800

    Added comments to code and cleaned it up a bit

--- Homework_13/FindTheNames.cs
+++ Homework_13/FindTheNames.cs
@@ -9,7 +9,6 @@ namespace Homework_13
 {
     class FindTheNames
     {
-
         private string[] boyNames = new string[1000];
         private string[] girlNames = new string[1000];
         private int[] boyNumber = new int[1000];
@@ -17,46 +16,67 @@ namespace Homework_13
 
         public void readFile(string fileName)
         {
+            
+            // values that help break up the file
             string[] names = new string[1000];
             int[] total = new int[1000];
             char[] delimiterChars = { ' ' };
+            // try the code below, if something goes wrong then throw an error
             try
             {
-
+                // open the file passed into the function
                 using (StreamReader sr = new StreamReader(fileName))
                 {
+                    // keep track of how many lines there are
                     int counter = 0;
+                    // value to read the line
                     string line = null;
+                    // while the text file is not null keep going
                     while ((line = sr.ReadLine()) != null)
                     {
+                        // the array words will hold each value from the text file that's seperated with a space
                         string[] words = line.Split(delimiterChars);
+                        // names will be stored in the names array
                         names[counter] = words[0];
+                        // the id will be stored in the total array
                         total[counter] = Int32.Parse(words[1]);
+                        // counter increments by one so it can be assigned to different values in each arrray
                         counter++;
                     }
                 }
-            } catch (FileNotFoundException e)
+            }
+            catch (FileNotFoundException e)
             {
+                // error displayed if file cannot be opened
                 Console.WriteLine("File cannot be found.");
             }
         }
 
+        // this function will look at the value in each array
         public void getNames()
         {
+            // prompts the user for a name
             Console.WriteLine("Enter in a name you would like to check: ");
+            // stores the value into an empty string
             string name = Console.ReadLine();
+            // if the array boyNames has the name in it
             if (this.boyNames.Contains(name))
             {
+                // look up the number the name the user searched for appears at in the array and store in index
                 int index = Array.IndexOf(boyNames, name);
+                // check the amount of times the name was used and store in occurences
                 int occurences = boyNumber[index];
+                // display the results to the user
                 Console.WriteLine(name + " is ranked number " + index + " among boy names with " + occurences);
             }
             else
             {
+                // otherwise the name could not be found
                 Console.WriteLine("That name was not found in the list provided.");
             }
 
-            if(this.girlNames.Contains(name))
+            // this will function the same as the array of boys above, just with the girls array
+            if (this.girlNames.Contains(name))
             {
                 int index = Array.IndexOf(girlNames, name);
                 int occurences = girlNumber[index];
--- Homework_13/Program.cs
+++ Homework_13/Program.cs
@@ -5,6 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * This program will open two txt files and find the name the user type in and return the names popularity
+ * Name: Spencer Unitt
+ * Module: Homework 13
+ */
+
 namespace Homework_13
 {
 
@@ -14,73 +20,15 @@ namespace Homework_13
        
         static void Main(string[] args)
         {
+            // functions that will help clean up the code for the main
+            // calling readFile should open and read the file and assign the values to an array
+            // getNames should return the name the user types in
             FindTheNames find = new FindTheNames();
             find.readFile("boynames.txt");
             find.readFile("girlnames.txt");
             find.getNames();
 
             Console.ReadLine();
-        }
-
-
-        private string[] boyNames = new string[1000];
-        private string[] girlNames = new string[1000];
-        private int[] boyNumber = new int[1000];
-        private int[] girlNumber = new int[1000];
-
-        public void readFile(string fileName)
-        {
-            string[] names = new string[1000];
-            int[] total = new int[1000];
-            char[] delimiterChars = { ' ' };
-            try
-            {
-
-                using (StreamReader sr = new StreamReader(fileName))
-                {
-                    int counter = 0;
-                    string line = null;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string[] words = line.Split(delimiterChars);
-                        names[counter] = words[0];
-                        total[counter] = Int32.Parse(words[1]);
-                        counter++;
-                    }
-                }
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine("File cannot be found.");
-            }
-        }
-
-        public void getNames()
-        {
-            Console.WriteLine("Enter in a name you would like to check: ");
-            string name = Console.ReadLine();
-            if (this.boyNames.Contains(name))
-            {
-                int index = Array.IndexOf(boyNames, name);
-                int occurences = boyNumber[index];
-                Console.WriteLine(name + " is ranked number " + index + " among boy names with " + occurences);
-            }
-            else
-            {
-                Console.WriteLine("That name was not found in the list provided.");
-            }
-
-            if (this.girlNames.Contains(name))
-            {
-                int index = Array.IndexOf(girlNames, name);
-                int occurences = girlNumber[index];
-                Console.WriteLine(name + " is ranked number " + index + " among girl names with " + occurences);
-            }
-            else
-            {
-                Console.WriteLine("Name was not found in list.");
-            }
-
         }
 
     
